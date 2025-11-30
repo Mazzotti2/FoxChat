57575df04f6502aa2851a4beab730285300a1a51
Mazzotti2 <pdawidowski@interia.pl>
2025-11-30 11:45:19 +0100

    hotfix

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ from views.teleport_handler import *
 from views.message_handler import *
 import json
 
-def makeApp():
-	app = Flask(__name__)
-	app.register_blueprint(login_blueprint)
-	app.register_blueprint(home_blueprint)
-	
-	return app
+app = Flask(__name__)
+
+app.register_blueprint(login_blueprint)
+app.register_blueprint(home_blueprint)
 
 if __name__ == "__main__":
-	app = makeApp()
 	app.run()
